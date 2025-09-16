2e2000f21c3d850d424faa5a66d5e85e7facb037
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-09-16 18:47:14 +0800

    fixed undefines "BOT"

--- cogs/commands.py
+++ cogs/commands.py
@@ -13,10 +13,10 @@ class Commands(commands.Cog):
         await self.bot.process_commands(msg)
         
     @commands.command
-    async def showCmd(ctx): # for sending a guide
+    async def showCmd(self, ctx): # for sending a guide
 
         embed = discord.Embed( title="📍 Class Companion Commands", description='''Use the `//` prefix before the command ''',color=discord.Colour.og_blurple())
-        embed.set_footer(text="Class Companion", icon_url=bot.user.avatar)
+        embed.set_footer(text="Class Companion", icon_url=self.bot.user.avatar)
         embed.add_field(name="", value="`schedule`", inline=True)
         embed.add_field(name="", value="`viewSchedule`", inline=True)
         embed.add_field(name="", value="`updateSchedule`", inline=True)
@@ -25,7 +25,7 @@ class Commands(commands.Cog):
         await ctx.send(embed=embed)
         
     @commands.command
-    async def schedule(ctx, *args): # for schedule command
+    async def schedule(self, ctx, *args): # for schedule command
 
         if ctx.guild is not None:
             embed = discord.Embed(title=f"{ctx.author.display_name or ctx.author.name}",description=f"A message has been sent to you!", color=discord.Colour.og_blurple())
@@ -33,7 +33,7 @@ class Commands(commands.Cog):
 
         if not args:
             embed = discord.Embed(title='🗺️ Schedule Command Guide', description='**⟣┄ˑ◌ The command `schedule` should be followed by the User Schedule in `DST` format. `NOTE`, you can add multiple schedule separated with space**',color=discord.Colour.og_blurple())
-            embed.set_thumbnail(url=bot.user.avatar)
+            embed.set_thumbnail(url=self.bot.user.avatar)
             embed.add_field(name="", value="`𖥔 Day`:  ( Monday - Saturday )", inline=False)
             embed.add_field(name="", value=" `𖥔 Subject`:  Users Subject for that day", inline=False)
             embed.add_field(name="", value="`𖥔 Time`:  12-Hour AM/PM format", inline=False)
@@ -53,15 +53,15 @@ class Commands(commands.Cog):
             await ctx.author.send(embed=schedule_set)
 
     @commands.command    
-    async def viewSchedule():
+    async def viewSchedule(self):
         pass
     
     @commands.command
-    async def updateSchedule():
+    async def updateSchedule(self):
         pass    
 
     @commands.command    
-    async def setActivity(): # for sever owner or admins
+    async def setActivity(self): # for sever owner or admins
         pass
 
 async def setup(bot):
