76f5c1a6308c6a49c12c14b8d6dfd7add0c43d29
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-09-23 20:49:43 +0800

    added timer

---
+++ cogs/reminder.py
@@ -0,0 +1,30 @@
+import pytz
+from datetime import datetime, time
+from discord.ext import tasks, commands
+
+class Reminder(commands.Cog):
+
+    ph_time = pytz.timezone('Asia/Manila')
+
+    def __init__ (self, bot):
+        self.bot = bot
+        self.time_checker.start()
+        self.stopper = False
+
+    def cog_unload(self):
+        self.time_checker.cancel()
+
+    @tasks.loop(minutes=1)
+    async def time_checker(self):
+        time, _ = datetime.now(self.ph_time).strftime("%I:%M %p").split(' ')
+        hours, __ = time.split(':')
+
+        if int(hours) == 8 and not self.stopper:
+            self.stopper = True
+            print('Working')
+
+        if not int(hours) == 0:
+            self.stopper = False
+
+async def setup(bot):
+    await bot.add_cog(Reminder(bot))
