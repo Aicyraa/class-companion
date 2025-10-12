6eab07eb59b5d7ff6af7bffc58036cb0b779bfa7
BFP <182109647+Aicyraa@users.noreply.github.com>
2025-10-12 11:37:01 +0800

    reformat the code

--- utils/sql_func_reminder.py
+++ utils/sql_func_reminder.py
@@ -13,13 +13,15 @@ class Reminder_Query:
 
         cnx = Settings.connection()
         cursor = cnx.cursor()
-        cursor.execute(f'USE {Settings.db}')
 
-        cursor.execute('SELECT user_discord_id FROM user')
-        result = {}
         today = datetime.now(Reminder_Query.ph_time).strftime("%A")
         
+        result = {}
+        
         try:
+            cursor.execute(f'USE {Settings.db}')
+            cursor.execute('SELECT user_discord_id FROM user')
+            
             for (user,) in cursor.fetchall():
                 storage = []
                 cursor.execute(
@@ -28,28 +30,25 @@ class Reminder_Query:
                     INNER JOIN user
                     ON schedules.user_discord_id = user.user_discord_id
                     WHERE schedules.user_discord_id = %s
-                    AND schedules.event_day = %s; 
-                    """, (user, today))
-
-                user_schedule = list(cursor.fetchall())  
+                    AND schedules.event_day = %s; """, (user, today))
                 
-                if not user_schedule:
+                for *details, time in list(cursor.fetchall()):
+                    details.append(tc.convert_to_12(time))
+                    print('details => ', details)
+                    storage.append(details)
+                
+                print('storage => ', storage)
+                if not storage: # prevents from sending notification if there is no schedule
                     continue
                 
-                for details in user_schedule:
-                    schedules = list(details) # convert details into list
-                    time = tc.convert_to_12(schedules.pop(-1))
-
-                    schedules.append(time)
-                    storage.append(schedules)
-                    
-                result[user] = storage # added event to storage
+                result[user] = storage # added event to result dict
 
         except sql.Error as err: print(f'Error occur while fetching schedule: {err}')
         finally:
             cursor.close()
             cnx.close()
 
+        print('result => ', result)
         return result
     
     @staticmethod
@@ -58,24 +57,27 @@ class Reminder_Query:
         cnx = Settings.connection()
         cursor = cnx.cursor()
         
-        cursor.execute(f'USE {Settings.db}')
-        cursor.execute('SELECT guild_id FROM guilds')
-
         result = {}   
-
+        
         try:
-            for (guild_id,) in cursor.fetchall(): 
+            cursor.execute(f'USE {Settings.db}')
+            cursor.execute('SELECT guild_id FROM guilds')
+            
+            for (guild_id,) in cursor.fetchall():
                 storage = []
                 cursor.execute(
                     '''
                     SELECT activities.activity_details, activities.expiry_date FROM activities
                     INNER JOIN guilds
-                    ON activities.guild_id = guilds.guild_id;
-                    ''')
+                    ON activities.guild_id = guilds.guild_id
+                    WHERE activities.guild_id = %s; ''', (guild_id, ))
             
                 for event in cursor.fetchall():
                     storage.append(event)
 
+                if not storage:
+                    continue
+                
                 result[guild_id] = storage # added event in result dict
             
         except sql.Error as err: print(f'Error occur while fetching activities: {err}')
@@ -91,9 +93,8 @@ class Reminder_Query:
         cnx = Settings.connection()
         cursor = cnx.cursor()
 
-        cursor.execute(f'''USE {Settings.db}''')
-        print('Check')
         try: 
+            cursor.execute(f'''USE {Settings.db}''')
             cursor.execute('''DELETE FROM activities WHERE expiry_date <= NOW(); ''')
             cnx.commit()
         except sql.Error as err: print(f'Error occur while deleting {err}')
